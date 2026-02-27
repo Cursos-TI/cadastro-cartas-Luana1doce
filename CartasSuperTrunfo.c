bf8f4f411d2c675faa6e96c677f8714ce14b7fe3
Luana1doce <lulu.lualinda123@gmail.com>
2026-02-27 00:25:16 -0300

    Enhance game logic and user interface
    Refactor game rules and improve user prompts for clarity. Add functionality for players to choose attributes and compare them during the game.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
- 
+
 
   int main()
-  // Desafio Jogo Super Trunfo - Países
-  // Objetivo: Simular cadastro de cartas por meio de entrada e saída de dados.
-  
+// Jogo Super Trunfo - Estados
+//O jogo funciona da seguinte forma: Dois jogadores entrarão com os dados das cartas da qual querem compedir.O sistema fornecerá um menu switch para que o jogador possa escolher dois atributos para competir. O sistema fará a comparação dos dois atributos através de calculos com operadores matemáticos, comparando os dois atributos, um de cada vez.
+//O sistema exibirá coforme lógica o ganhador e depois entregará a comparação de todos os atributos e qual a carta mais poderosa.
+//A  comparação de todos os atributos e a carta mais poderosa, são apenas para fins informativos e não alteram quem venceu o jogo!
 {
-    // Variáveis Carta 1
+ // Variáveis Carta 1
      
      char estado= 'S';
      char codigo[8]= "S01";
@@ -18,16 +19,43 @@
      float pib_percapta;
      float densidade_populacional;
 
-    
-    
-    //Entrada de Dados Carta 1
+int menu_inicial;
+printf("Bem Vindo ao Super Trunfo Cidades!\n");
+printf(" Pronto para competir ?\n");
+printf("Digite 1 para Continuar\n");
+printf("Digite 2 para saber as regras do jogo\n");
+scanf("%d",&menu_inicial);  
 
-      printf("Digite os dados das cartas\n");
+switch (menu_inicial)
+{
+case 1: 
+printf(" ok! Vamos começar. Digite os dados das cartas\n");
+break;
+case 2:
+printf("  O jogo funciona da seguinte forma:\n");
+printf("Dois jogadores escolherão cuidadosamente uma cidade para competir. Cada jogador deve escolher uma cidade individualmente;\n");
+printf("  Os Jogadores de comum acordo poderão escolher duas características da cidade para competir; \n");
+printf(" Serão duas partidas, uma para cada característica.\n");
+printf(" O primeiro Jogador a digitar os dados da cidade será denominado JOGADOR 1e o segundo JOGADOR 2;n");
+printf(" O jogador que tiver os dois atributos mais fortes vence! Mas atenção! Nem sempre o atributo maior é o mais forte.\n");
+printf("  No final será exibido para fins de análise estratégica a comparação de cada característica das cidades juntamente com o SUPER PODER que o ajudará a avaliar a cidade com atributos mais fortes.\n");
+printf(" AGORA JÁ SABEMOS AS REGRAS! \n");
+printf(" PRONTO PARA COMPETIR?.\n");
+printf(" Digite os dados da cidade:\n");
+
+
+
+break;
+default:
+printf (" Opção Inválida! Escolha uma opção entre 1 e 2.\n");
+printf("Digite aqui:\n  ");
+scanf("  %d",&menu_inicial);  
+}
     
-      printf("Estado:\n");
-      scanf ("%c", &estado);
+      printf(" Estado:\n ");
+      scanf (" %c", &estado);
     
-      printf("Código:\n");
+      printf(" Código:\n");
       scanf ("%s", &codigo);
     
       printf("Cidade:\n");
@@ -47,6 +75,7 @@
 
 
     //Calculo PIB PERCAPTA e Densidade Populacional Carta 2
+// Esses são atributos calculados pelo próprio sistema e que agregam valor a carta do jogador!
 
     
         pib_percapta= pib/populacao;
@@ -118,9 +147,9 @@
     //Impressão 
    
    
-    printf ("Carta 2:\n");
-    printf ("Estado: %c \n",estado1);
-    printf ("Codigo: %s \n",codigo1);
+    printf ("Carta 2:\n ");
+    printf (" Estado: %c \n ",estado1);
+    printf (" Codigo: %s \n",codigo1);
     printf ("Cidade: %s \n",cidade1);
     printf ("População:%d \n",populacao1);
     printf ("Área:%.1f \n",area1);
@@ -129,8 +158,20 @@
     printf ("PIB Percapta:%f\n",pib_percapta1);
     printf ("Densidade Populacional:%f\n",densidade_populacional1);
 
-//Menu para primeiro jogador
-int opcao;
+//Este é o menu da qual o jogador escolherá dois atributos, um de cada vez para competir com o jogador rival.
+//Nestas variáveis ficarão armazenados os pontos dos jogadores!
+int pontos_jogador1= 0;
+int pontos_jogador2= 0;
+//Início do Menu de Opções.
+
+int opcao1;
+int opcao2;
+int invalido1= 0;
+int invalido2=0;
+//Uso do IF para evitar a repetição da opção escolhida.
+if(opcao1==opcao2)
+{printf(" Você ja escolheu essa opção.Escolha uma opção diferente!\n");}
+
 printf( "ATENÇÃO ! Escolha um atributo para competir!\n");
 printf(" DIGITE: \n");
 printf("1 para População;\n");
@@ -140,65 +181,216 @@ printf( " 4 para Pontos Turísticos;\n");
 printf("5 para PIB Percapta;\n");
 printf(" 6 para Densidade Populacional;\n");
 printf("Digite aqui..........");
-scanf("%d", &opcao);
+scanf("%d", &opcao1);
 
-switch (opcao){
+switch (opcao1){
 
 case 1: 
 printf( "Você escolheu População!");
-if( populacao>=populacao1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( populacao>populacao1)
+{ printf ( "Jogador 1 ganhou!%d\n");
+pontos_jogador1++;}
+else
+if( populacao==populacao1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
 break;
 
 
 case 2:
 printf( "Você escolheu Área!");
-if( area>=area1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( area>area1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( area==area1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
 break;
 
 case 3:
 printf( "Você escolheu PIB!");
-if( pib>=pib1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( pib>pib1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pib==pib1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
 break;
 
 case 4:
 printf( "Você escolheu Pontos Turísticos!");
-if( pontos_turisticos>=pontos_turisticos1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( pontos_turisticos>pontos_turisticos1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pontos_turisticos==pontos_turisticos1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
 break;
 
 case 5:
 printf( "Você escolheu PIB Percapta!");
-if( pib_percapta>=pib_percapta1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( pib_percapta>pib_percapta1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pib_percapta==pib_percapta1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
 break;
 
 case 6:
 printf(" Voce escolheu Densidade Populacional!");
-if( densidade_populacional>=densidade_populacional1)
-{ printf ( "Jogador 1 ganhou!\n");}
-else { printf("Jogador 2 ganhou!\n");}
+if( densidade_populacional<densidade_populacional1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pib_percapta==pib_percapta1)
+{printf("Empate!");}
+else { printf("Jogador 2 ganhou! \n");
+pontos_jogador2++;}
 break;
-default: printf("Opção inválida!");}
- 
+default: printf("Opção inválida!");
+invalido1++;
+}
 
 
+printf( "ATENÇÃO ! Escolha um atributo para competir!\n");
+printf(" DIGITE: \n");
+printf("1 para População;\n");
+printf("2 para Área;\n");
+printf("3 para PIB;\n");
+printf( " 4 para Pontos Turísticos;\n");
+printf("5 para PIB Percapta;\n");
+printf(" 6 para Densidade Populacional;\n");
+printf("Digite aqui..........");
+scanf("%d", &opcao2);
+
+switch (opcao2){
+
+case 1: 
+printf( "Você escolheu População!");
+if( populacao>populacao1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( populacao==populacao1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
+break;
+
+
+case 2:
+printf( "Você escolheu Área!");
+if( area>area1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( area==area1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
+break;
+
+case 3:
+printf( "Você escolheu PIB!");
+if( pib>pib1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pib==pib1)
+{printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
+break;
+
+case 4:
+printf( "Você escolheu Pontos Turísticos!");
+if( pontos_turisticos>pontos_turisticos1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pontos_turisticos==pontos_turisticos1)
+{ printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
+break;
+
+case 5:
+printf( "Você escolheu PIB Percapta!");
+if( pib_percapta>pib_percapta1) 
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else
+if( pib_percapta==pib_percapta1)
+{ printf("Empate!");}
+
+else { printf("Jogador 2 ganhou!\n");
+pontos_jogador2++;}
+break;
+
+case 6:
+printf(" Você escolheu Densidade Populacional!");
+if( densidade_populacional<densidade_populacional1)
+{ printf ( "Jogador 1 ganhou!\n");
+pontos_jogador1++;}
+else  
+if(densidade_populacional == densidade_populacional1)
+{ printf("Empate!");}
+else { printf("Jogador 2 ganhou! \n");
+pontos_jogador2++;}
+printf("Escolha outro atributo!\n");
+break;
+default: printf("Opção inválida!\n");
+invalido2++; }
+
+ //Lógica para apontar ganhador usando if-else + operador ternário.
+
+
+if (invalido1 > 0 || invalido2 > 0)
+{
+    printf("Opções Inválidas! Reinicie o Jogo.\n");
+}
+else if (pontos_jogador1 == pontos_jogador2)
+{
+    printf("Empate Geral!\n");
+}
+else if (pontos_jogador1 > pontos_jogador2)
+{
+    printf("Jogador 1 Ganhou!\n Código da cidade: %s\n", cidade);
+}
+else
+{
+    printf("Jogador 2 Ganhou!\n Código da cidade: %s\n", cidade1);
+}
  //Comparação de atributos
-
+//Variáveis para armazenar os atributos
+printf("PARA AS PRÓXIMAS JOGADAS!\n");
+printf(" Veja uma comparação entre as características(ATRIBUTOS) das cidades e quais os mais fortes!\n");
   float atributo_pib;            
   float atributo_pibpercapta;
   float atributo_area;
   float atributo_populacao;
   int atributo_pontos_turisticos;
   float atributo_densidade_populacional;
-
+//Calculo dos atributos das cartas
 atributo_pib=pib>pib1;
 atributo_pibpercapta=pib_percapta>pib_percapta1;
 atributo_area=area>area1;
@@ -215,28 +407,27 @@ atributo_densidade_populacional=densidade_populacional<densidade_populacional1;
   printf("Densidade Populacional vencedora%f\n",atributo_densidade_populacional);
 
 
- 
-  //Variáveis Super Poder
+//Nessa parte do jogo o sistema entregará qual carta é mais poderosa!
+//Variáveis Super Poder
+printf(" Com base nas características das cartas veja qual a mais poderosa!\n");
     float super_poder_carta1;
     float super_poder_carta2;
     int resultado_vencedor;
 
-
-    //Calculo Super Poder
+ //Calculo Super Poder
         
-        super_poder_carta1=(pib+area+populacao+pontos_turisticos+pib_percapta)-densidade_populacional;
-        super_poder_carta2=(pib1+area1+populacao1+pontos_turisticos1+pib_percapta1)-densidade_populacional1;
+super_poder_carta1=(pib+area+populacao+pontos_turisticos+pib_percapta)-densidade_populacional;
+super_poder_carta2=(pib1+area1+populacao1+pontos_turisticos1+pib_percapta1)-densidade_populacional1;
 
 
-  printf("Super Poder Carta 1:%f\n",super_poder_carta1);
+ printf("Super Poder Carta 1:%f\n",super_poder_carta1);
 
-  printf("Super Poder Carta 2:%f\n",super_poder_carta2);
+ printf("Super Poder Carta 2:%f\n",super_poder_carta2);
  
 
  if (super_poder_carta1 > super_poder_carta2) {
-    printf("Ual Carta 1 venceu\n");
-} else {
-    printf("Ual Carta 2 venceu\n");
-}
+    printf("A cidade 1 é mais forte!\n");}
+ else { printf("A cidade 2 é mais forte!\n");}
+printf("Grandes decisões constroem grandes vitórias.");
    return 0;
 }
